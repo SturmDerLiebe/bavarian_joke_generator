a549931078bfcd2ec75fda64a61925ce057d1e21
Christian Sturm <christiansturm.dachau@web.de>
2023-10-03 20:02:35 +0200

    Feat(Constants): Add db constants for login

---
+++ constants/db.js
@@ -0,0 +1,18 @@
+"use strict"
+
+/**
+  * The user to access the database
+  * @readonly
+  * @enum {string}
+  */
+const ROLES = {
+  /** User with READ privileges only */
+  reader: "Reader"
+};
+
+/**
+  * The main database
+  * @readonly
+  */
+const DATABASE_NAME = "Bavarian_Jokes";
+export { ROLES, DATABASE_NAME };
