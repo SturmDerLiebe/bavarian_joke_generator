2b1a5acdbe0649a4f01106bb951ddfeb2ece831f
Christian Sturm <christiansturm.dachau@web.de>
2023-10-07 17:52:37 +0200

    Fix(Database): Add quotes and remove role with duplicate name

--- database/mysql/1_jokes_accounts.sql
+++ database/mysql/1_jokes_accounts.sql
@@ -1,7 +1,4 @@
-CREATE ROLE IF NOT EXISTS reader;
-
-GRANT SELECT ON bavarian_jokes.joke TO reader;
-GRANT SELECT ON bavarian_jokes.jk_pair TO reader;
-GRANT SELECT ON bavarian_jokes.keyword TO reader;
-
-CREATE USER IF NOT EXISTS reader IDENTIFIED BY 'readerpw' DEFAULT ROLE reader; 
+CREATE USER IF NOT EXISTS 'reader' IDENTIFIED BY 'test'; 
+GRANT SELECT ON bavarian_jokes.joke TO 'reader';
+GRANT SELECT ON bavarian_jokes.jk_pair TO 'reader';
+GRANT SELECT ON bavarian_jokes.keyword TO 'reader';
