129af86d96c5859cb1af6f8806e90ac02040c918
Christian Sturm <christiansturm.dachau@web.de>
2023-10-12 20:34:31 +0200

    Fix(API): Fix import, await and missing port listener

--- api/user/main.js
+++ api/user/main.js
@@ -1,14 +1,14 @@
 "use strict"
 
 import express from "express";
-import db_read_joke from "../../helpers/database/read_joke_db";
+import db_read_joke from "../../helpers/database/read_joke_db.js";
 
 const APP = express();
 
 APP.set('view engine', 'pug')
 APP.get('/:keyword', async function(req, res) {
   const KEYWORD = req.params.keyword;
-  const JOKE_DATA = db_read_joke(KEYWORD);
+  const JOKE_DATA = await db_read_joke(KEYWORD);
   res.render('keyword', { KEYWORD, JOKE_DATA })
 })
 
@@ -23,3 +23,7 @@ APP.get("api/read_joke/:keyword", function(req, res) {
     res.writeHead(404, `No joke with the keyword ${KEYWORD} was found`).end();
   }
 });
+
+APP.listen(8000, () => {
+  console.log(`Example app listening on port 8000`)
+})
