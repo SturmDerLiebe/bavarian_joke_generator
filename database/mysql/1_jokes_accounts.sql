3b227dace242d7392673fe726d082deb3286371a
Christian Sturm <christiansturm.dachau@web.de>
2023-10-18 20:29:40 +0200

    Feat(Database): Add new submitter user and adjust dev passwords

--- database/mysql/1_jokes_accounts.sql
+++ database/mysql/1_jokes_accounts.sql
@@ -1,4 +1,9 @@
-CREATE USER IF NOT EXISTS 'reader' IDENTIFIED BY 'test'; 
+CREATE USER IF NOT EXISTS 'reader' IDENTIFIED BY 'readerPW'; 
 GRANT SELECT ON bavarian_jokes.joke TO 'reader';
 GRANT SELECT ON bavarian_jokes.jk_pair TO 'reader';
 GRANT SELECT ON bavarian_jokes.keyword TO 'reader';
+
+CREATE USER IF NOT EXISTS 'submitter' IDENTIFIED BY 'submitterPW'; 
+GRANT SELECT ON bavarian_jokes.joke TO 'submitter';
+GRANT SELECT ON bavarian_jokes.jk_pair TO 'submitter';
+GRANT SELECT ON bavarian_jokes.keyword TO 'submitter';
