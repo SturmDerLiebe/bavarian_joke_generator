4d79e4700e61dd1d5aacc41ad5511911e63d10bc
Christian Sturm <christiansturm.dachau@web.de>
2023-10-18 20:34:14 +0200

    Test(Helpers/Database): "Use implemented write to db function"

--- helpers/database/read_joke_db.test.js
+++ helpers/database/read_joke_db.test.js
@@ -3,7 +3,7 @@
 import { expect, test, describe } from "@jest/globals";
 // Internal:
 import db_read_joke from "./read_joke_db.js";
-
+import db_create_joke from "./create_joke_db.js";
 const KEYWORD = "Dummy";
 describe("db_read_joke", function () {
   test(", given an existing keyword, returns all jokes' data in an array", async function () {
@@ -11,8 +11,8 @@ describe("db_read_joke", function () {
     const DUMMY_JOKE = "Lorem Ipsum";
     const DUMMY_EXPLANATION = "This explains everything";
     const USER = "Mark";
-    await write_joke_db(DUMMY_JOKE, DUMMY_EXPLANATION, KEYWORD);
-    await write_joke_db(DUMMY_JOKE, DUMMY_EXPLANATION, KEYWORD, USER);
+    await db_create_joke(DUMMY_JOKE, DUMMY_EXPLANATION, KEYWORD);
+    await db_create_joke(DUMMY_JOKE, DUMMY_EXPLANATION, KEYWORD, USER);
 
     // WHEN the function is called:
     const JOKE_DATA = await db_read_joke(KEYWORD);
