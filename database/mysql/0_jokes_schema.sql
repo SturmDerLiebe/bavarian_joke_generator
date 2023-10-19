09ffa830ac80c0d9940be2ddc50d868691ae39ac
Christian Sturm <christiansturm.dachau@web.de>
2023-10-19 20:15:42 +0200

    Fix(Database): Fix inconsistent schema attributes

--- database/mysql/0_jokes_schema.sql
+++ database/mysql/0_jokes_schema.sql
@@ -2,57 +2,56 @@ CREATE DATABASE IF NOT EXISTS bavarian_jokes;
 
 USE bavarian_jokes;
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE
+  IF NOT EXISTS user (
     id SERIAL,
     username VARCHAR(30) NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE KEY (username)
-);
+    UNIQUE (username)
+  );
 
 CREATE TABLE
-IF NOT EXISTS authenticator (
+  IF NOT EXISTS authenticator (
     id SERIAL,
-    credentialid TEXT,
-    credentialpublickey BLOB,
+    credentialid TEXT NOT NULL,
+    credentialpublickey BLOB NOT NULL,
     counter BIGINT UNSIGNED NOT NULL,
-    credentialdevicetype ENUM('singleDevice', 'multiDevice') NOT NULL,
+    credentialdevicetype ENUM ('singleDevice', 'multiDevice') NOT NULL,
     credentialbackedup BOOL NOT NULL,
     user_id BIGINT UNSIGNED NOT NULL,
     transports
-    SET(
-        'ble', 'cable', 'hybrid', 'internal', 'nfc', 'smart-card', 'usb'
-    ) NOT NULL DEFAULT '',
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES user (
-        id
-    ) ON DELETE CASCADE ON UPDATE CASCADE
-);
+    SET
+      (
+        'ble',
+        'cable',
+        'hybrid',
+        'internal',
+        'nfc',
+        'smart-card',
+        'usb'
+      ) DEFAULT NULL,
+      PRIMARY KEY (id),
+      FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE
+  );
 
 CREATE TABLE
-IF NOT EXISTS joke (
+  IF NOT EXISTS joke (
     id SERIAL,
     content TEXT NOT NULL,
     explanation TEXT NOT NULL,
-    submitted_by BIGINT UNSIGNED,
+    submitted_by BIGINT UNSIGNED DEFAULT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (submitted_by) REFERENCES user (
-        id
-    ) ON DELETE SET NULL ON UPDATE CASCADE
-);
+    FOREIGN KEY (submitted_by) REFERENCES user (id) ON DELETE SET NULL ON UPDATE CASCADE
+  );
 
 CREATE TABLE
-IF NOT EXISTS keyword (id SERIAL, title VARCHAR(30) NOT NULL, PRIMARY KEY (id));
+  IF NOT EXISTS keyword (name VARCHAR(30) NOT NULL, PRIMARY KEY (name));
 
 CREATE TABLE
-IF NOT EXISTS jk_pair (
+  IF NOT EXISTS jk_pair (
     joke_id BIGINT UNSIGNED NOT NULL,
-    keyword_id BIGINT UNSIGNED NOT NULL,
-    PRIMARY KEY (joke_id, keyword_id),
-    FOREIGN KEY (joke_id) REFERENCES joke (
-        id
-    ) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (keyword_id) REFERENCES keyword (
-        id
-    ) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
+    keyword_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (joke_id, keyword_name),
+    FOREIGN KEY (joke_id) REFERENCES joke (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (keyword_name) REFERENCES keyword (name) ON DELETE CASCADE ON UPDATE CASCADE
+  );
