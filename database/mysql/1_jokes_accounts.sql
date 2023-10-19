57af9d5322c548456bfb92eea5549b5298997470
Christian Sturm <christiansturm.dachau@web.de>
2023-10-19 20:09:23 +0200

    Feat(Database): Add logged_in_user User

--- database/mysql/1_jokes_accounts.sql
+++ database/mysql/1_jokes_accounts.sql
@@ -1,9 +1,27 @@
-CREATE USER IF NOT EXISTS 'reader' IDENTIFIED BY 'readerPW'; 
-GRANT SELECT ON bavarian_jokes.joke TO 'reader';
-GRANT SELECT ON bavarian_jokes.jk_pair TO 'reader';
-GRANT SELECT ON bavarian_jokes.keyword TO 'reader';
+-- READER:
+CREATE USER IF NOT EXISTS 'reader' IDENTIFIED BY 'readerPW';
 
-CREATE USER IF NOT EXISTS 'submitter' IDENTIFIED BY 'submitterPW'; 
-GRANT SELECT ON bavarian_jokes.joke TO 'submitter';
-GRANT SELECT ON bavarian_jokes.jk_pair TO 'submitter';
-GRANT SELECT ON bavarian_jokes.keyword TO 'submitter';
+GRANT
+SELECT
+  ON bavarian_jokes.joke TO 'reader';
+
+GRANT
+SELECT
+  ON bavarian_jokes.jk_pair TO 'reader';
+
+GRANT
+SELECT
+  ON bavarian_jokes.keyword TO 'reader';
+
+-- SUBMITTER:
+CREATE USER IF NOT EXISTS 'submitter' IDENTIFIED BY 'submitterPW';
+
+GRANT INSERT ON bavarian_jokes.joke TO 'submitter';
+GRANT INSERT ON bavarian_jokes.jk_pair TO 'submitter';
+GRANT INSERT ON bavarian_jokes.keyword TO 'submitter';
+
+-- LOGGED_IN_USER:
+CREATE USER IF NOT EXISTS 'logged_in_user' IDENTIFIED BY 'logged_in_userPW';
+
+GRANT DELETE,
+UPDATE (content, explanation) ON bavarian_jokes.joke TO 'logged_in_user';
