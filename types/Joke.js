eb1a5e78f4ebbdfde8bec3d7712ca2c62cf87ce7
Christian Sturm <christiansturm.dachau@web.de>
2023-10-20 14:48:17 +0200

    Fix(Types): Anonymous submissions have a null user now

--- types/Joke.js
+++ types/Joke.js
@@ -14,7 +14,7 @@
  * @typedef {Object} Joke_Submission
  * @property {string} content - The content of this joke.
  * @property {string} explanation - The explanation if this joke.
- * @property {string|""} submitted_by - The user id of the submitter. An empty string signifies an anonymous submission.
+ * @property {string|null} submitted_by - The user id of the submitter. Null signifies an anonymous submission.
  * @property {string[]} keywords - The keywords associated with the submitted joke.
  */
 
