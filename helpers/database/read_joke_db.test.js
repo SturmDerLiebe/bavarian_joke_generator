f3f7704b2efced42d2d28f804ce0633ca93d18a4
Christian Sturm <christiansturm.dachau@web.de>
2023-10-20 14:51:05 +0200

    Test(Helpers/Database): Incorporate new test constants

--- helpers/database/read_joke_db.test.js
+++ helpers/database/read_joke_db.test.js
@@ -4,34 +4,34 @@ import { expect, test, describe } from "@jest/globals";
 // Internal:
 import db_read_joke from "./read_joke_db.js";
 import db_create_joke from "./create_joke_db.js";
-const KEYWORD = "Dummy";
+// Constants
+import {
+  CREATE_ARGS_ANONYMOUS,
+  CREATE_ARGS_BYUSER,
+  EXISTING_KEYWORD,
+  JOKE_RESULT,
+  NONEXISTING_KEYWORD,
+} from "../../constants/test.js";
+
 describe("db_read_joke", function () {
   test(", given an existing keyword, returns all jokes' data in an array", async function () {
     // GIVEN the keyword exists in the db and has two jokes associated with it:
-    const DUMMY_JOKE = "Lorem Ipsum";
-    const DUMMY_EXPLANATION = "This explains everything";
-    const USER = "Mark";
-    await db_create_joke(DUMMY_JOKE, DUMMY_EXPLANATION, KEYWORD);
-    await db_create_joke(DUMMY_JOKE, DUMMY_EXPLANATION, KEYWORD, USER);
+    await Promise.all([
+      db_create_joke(CREATE_ARGS_BYUSER),
+      db_create_joke(CREATE_ARGS_ANONYMOUS),
+    ]);
 
     // WHEN the function is called:
-    const JOKE_DATA = await db_read_joke(KEYWORD);
+    const JOKE_DATA = await db_read_joke(EXISTING_KEYWORD);
 
     // THEN it should return an array of two joke data objects:
-    const DUMMY_DATA1 = {
-      id: expect.any(String),
-      text: DUMMY_JOKE,
-      explanation: DUMMY_EXPLANATION,
-      submitted_by: null,
-    };
-    const DUMMY_DATA2 = { ...DUMMY_DATA1, submitted_by: USER };
-    expect(JOKE_DATA).toEqual([DUMMY_DATA1, DUMMY_DATA2]);
+    expect(JOKE_DATA).toEqual(expect.arrayContaining(JOKE_RESULT));
   });
 
   test(", given a keyword not present yet, returns null", async function () {
     // GIVEN the keyword does not exist
     // WHEN the function is called:
-    const JOKE_DATA = await db_read_joke(KEYWORD);
+    const JOKE_DATA = await db_read_joke(NONEXISTING_KEYWORD);
     // THEN it should return null:
     expect(JOKE_DATA).toHaveLength(0);
   });
