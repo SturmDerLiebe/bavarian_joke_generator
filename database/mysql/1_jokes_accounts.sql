6cd3778bcf764627e3e59fa6842073632438a0b7
Christian Sturm <christiansturm.dachau@web.de>
2023-10-20 14:45:39 +0200

    Fix(Database): Add missing priviledges

--- database/mysql/1_jokes_accounts.sql
+++ database/mysql/1_jokes_accounts.sql
@@ -1,28 +1,20 @@
 -- READER:
 CREATE USER IF NOT EXISTS reader IDENTIFIED BY 'readerPW';
 
-GRANT
-SELECT
-ON bavarian_jokes.joke TO reader;
-
-GRANT
-SELECT
-ON bavarian_jokes.jk_pair TO reader;
-
-GRANT
-SELECT
-ON bavarian_jokes.keyword TO reader;
+GRANT SELECT ON bavarian_jokes.joke TO reader;
+GRANT SELECT ON bavarian_jokes.jk_pair TO reader;
+GRANT SELECT ON bavarian_jokes.keyword TO reader;
+GRANT SELECT ON bavarian_jokes.user TO reader;
 
 -- SUBMITTER:
 CREATE USER IF NOT EXISTS submitter IDENTIFIED BY 'submitterPW';
 
 GRANT INSERT ON bavarian_jokes.joke TO submitter;
 GRANT INSERT ON bavarian_jokes.jk_pair TO submitter;
-GRANT INSERT ON bavarian_jokes.keyword TO submitter;
+GRANT SELECT, INSERT, UPDATE ON bavarian_jokes.keyword TO submitter;
 
 -- LOGGED_IN_USER:
 CREATE USER IF NOT EXISTS logged_in_user IDENTIFIED BY 'logged_in_userPW';
 
 GRANT DELETE,
 UPDATE (content, explanation) ON bavarian_jokes.joke TO logged_in_user;
-
