7a4c2a63dc7bccda14031424f08e78954de50460
Christian Sturm <christiansturm.dachau@web.de>
2023-10-20 14:54:36 +0200

    Fix(Helpers/Server): Use proper mysql client feature for transactions

--- helpers/database/create_joke_db.js
+++ helpers/database/create_joke_db.js
@@ -16,46 +16,75 @@ async function db_create_joke({
   submitted_by,
   keywords,
 }) {
+  // Set up connection and queries:
   const CONNECTION = await mysql.createConnection(
     get_connection_options("create"),
   );
+
+  /**
+   * A group of queries that insert only one joke, then insert the keywords if they are new and then inserts the joke-keyword pairs.
+   * @type {QP_Pair[]}
+   */
+  const QUERIES = [
+    [
+      "INSERT INTO joke(content, explanation, submitted_by) VALUES(?, ?, ?);",
+      [content, explanation, submitted_by], // Insert anonymous submitters (="") as NULL
+    ],
+    ["SET @j_id = LAST_INSERT_ID();"], // Save new joke_id for following inserts
+  ].concat(repeat_keyword_INSERT(keywords));
+
   try {
-    // The following Query inserts the joke, inserts the keywords if they are new
-    // and inserts the joke-keyword pairs.
-    const [RESULT_HEADER] = await CONNECTION.execute(
-      `
-        START TRANSACTION;
-          INSERT INTO joke (content, explanation, submitted_by) VALUES(?, ?, ?);
-          SET @j_id = LAST_INSERT_ID();
-          ${repeat_INSERT_for_keywords(keywords)}
-        COMMIT;
-      `,
-      [
-        content,
-        explanation,
-        !submitted_by ? null : user_id, // Insert anonymous submitters (="") as NULL
-        ...keywords, // spread keywords so that every single on gets prepared
-      ],
-    );
-    return RESULT_HEADER.insertId;
+    await CONNECTION.beginTransaction();
+    const [[JOKE_HEADER]] = await Promise.all(
+      execute_query(CONNECTION, QUERIES),
+    ); // Only interested in the into joke inserted id
+    await CONNECTION.commit();
+
+    return JOKE_HEADER.insertId;
+  } catch (error) {
+    await CONNECTION.rollback();
+    throw new Error(error);
   } finally {
     await CONNECTION.end();
   }
 }
 
 /**
- * @param {string[]} keywords - An array of keywords to be used in a query.
+ * Type of a Query-Parameter-Pair.
+ * @typedef {[string, string[]|undefined]} QP_Pair
  */
-function repeat_INSERT_for_keywords(keywords) {
-  return (sql_query = keywords
-    .map(function() {
-      return `
-      SET @k_title = ?;
-      INSERT INTO keyword (title) VALUES(@k_title) ON DUPLICATE KEY UPDATE title=title;
-      INSERT INTO jk_pair (joke_id, keyword_title) VALUES(@j_id, @k_title);
-      `;
-    })
-    .join(""));
+
+/**
+ * Executes multiple queries on a connection and returns all ongoing queries in an array.
+ * @param {mysql.Connection} connection - The connection to execute the queries on.
+ * @param {QP_Pair[]} queries - An array of queries to be executed.
+ * @returns {Promise<[import("mysql2").ResultSetHeader, import("mysql2").FieldPacket[]]>[]}
+ */
+function execute_query(connection, queries) {
+  return queries.map(function (query) {
+    const PARAMS = query[1];
+    return connection.execute(query[0], !PARAMS ? null : PARAMS);
+  });
+}
+
+/**
+ * Creates an array with the query-parameter-pairs for all keywords.
+ * @param {string[]} keywords - Keywords to be inserted.
+ * @returns {QP_Pair[]} queries - An array if queries that can be used for execution.
+ */
+function repeat_keyword_INSERT(keywords) {
+  return keywords.flatMap(function (keyword) {
+    return [
+      [
+        "INSERT INTO keyword (title) VALUES (?) ON DUPLICATE KEY UPDATE title=title;",
+        [keyword],
+      ],
+      [
+        "INSERT INTO jk_pair (joke_id, keyword_title) VALUES (@j_id, ?);",
+        [keyword],
+      ],
+    ];
+  });
 }
 
 export default db_create_joke;
