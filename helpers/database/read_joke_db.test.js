4580a6727d3ffb8753111d7c903364cd30a5b5f3
Christian Sturm <christiansturm.dachau@web.de>
2023-10-21 13:35:00 +0200

    Test(Helpers/Database): Differentiate between anon and non anon jokes

--- helpers/database/read_joke_db.test.js
+++ helpers/database/read_joke_db.test.js
@@ -19,8 +19,24 @@ describe("db_read_joke returns jokes", function () {
   test("of anonymous submitters", async function () {
     // GIVEN
     await Promise.all([
-      db_create_joke(CREATE_ARGS_BYUSER),
       db_create_joke(CREATE_ARGS_ANONYMOUS),
+      db_create_joke(CREATE_ARGS_ANONYMOUS),
+    ]);
+
+    // WHEN
+    const JOKE_DATA = await db_read_joke(EXISTING_KEYWORD);
+
+    // THEN it should return an array of two joke data objects:
+    expect(JOKE_DATA).toEqual(
+      expect.arrayContaining([ANONYMOUS_JOKE, ANONYMOUS_JOKE]),
+    );
+  });
+
+  test("of registered users", async function () {
+    // GIVEN
+    await Promise.all([
+      db_create_joke(CREATE_ARGS_BYUSER),
+      db_create_joke(CREATE_ARGS_BYUSER),
     ]);
 
     // WHEN
@@ -29,6 +45,7 @@ describe("db_read_joke returns jokes", function () {
     // THEN it should return an array of two joke data objects:
     expect(JOKE_DATA).toEqual(expect.arrayContaining([USER_JOKE, USER_JOKE]));
   });
+});
 
 describe("db_read_joke", function () {
   test("returns empty array for keywords not paired with a joke", async function () {
