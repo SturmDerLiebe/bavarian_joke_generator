2340a5c6daaf347c67ab264487229af2096d68ea
Christian Sturm <christiansturm.dachau@web.de>
2023-10-24 18:41:49 +0200

    Feat(Helpers/API): Add Keyword verification function

---
+++ helpers/api/get_joke.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Checks if the keyword only contains letters from the english alphabet.
+ * @param {string} keyword - The keyword to serch for jokes.
+ * @returns {boolean} - If the keyword is ok or not.
+ */
+function is_valid_keyword(keyword) {
+  const REGEX = new RegExp("(?<!.)[A-Za-z]+(?!.)");
+  return REGEX.test(keyword);
+}
+
+export { is_valid_keyword };
