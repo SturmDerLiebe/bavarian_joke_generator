9f2639a445967cc5ed31a2f0504c825b36e99ab2
Christian Sturm <christiansturm.dachau@web.de>
2023-11-03 18:56:17 +0100

    Test(): Add function to clear tables before Tests

---
+++ helpers/test/delete_jokes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import mysql from "mysql2/promise";
+// Constants:
+import { get_connection_options } from "../../constants/db.js";
+
+async function delete_all_jokes_and_keywords() {
+  const CONNECTION = await mysql.createConnection({
+    ...get_connection_options("read"),
+    host: process.env.DB_EXTERNAL_HOST,
+    user: "tester",
+    password: "testerPW",
+  });
+  try {
+    await CONNECTION.execute(`DELETE FROM joke;`);
+    await CONNECTION.execute(`DELETE FROM keyword;`);
+  } finally {
+    await CONNECTION.end();
+  }
+}
+
+export default delete_all_jokes_and_keywords;
