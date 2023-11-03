999ae932fe302b24130b2ae63320a2b3b55c8131
Christian Sturm <christiansturm.dachau@web.de>
2023-11-03 18:42:21 +0100

    Feat(Lib): Add joke not found error for 404

--- library/Errors.js
+++ library/Errors.js
@@ -13,4 +13,17 @@ class Client_Error extends Error {
   }
 }
 
-export { Client_Error };
+/**
+ * An extension class to filter 404 errors
+ * @extends Client_Error
+ */
+class Not_Found_Error extends Client_Error {
+  /**
+   * @param {string} message - The error message.
+   */
+  constructor(message) {
+    super(message);
+  }
+}
+
+export { Client_Error, Not_Found_Error };
