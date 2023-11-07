26b3a72c267a23ed226b8be436956a820e62df1e
Christian Sturm <christiansturm.dachau@web.de>
2023-11-07 23:45:24 +0100

    Test: fix wrong submission format

--- constants/playwright.js
+++ constants/playwright.js
@@ -2,26 +2,26 @@
 
 const VALID_KEYWORD = "Dog";
 const LISTED_KEYWORD = VALID_KEYWORD;
-const SINGLE_SUBMISSION = get_joke_times(1)[1];
+const SINGLE_SUBMISSION = get_joke_times(1)[0];
 const DUBLICATE_JOKE_SUBMISSIONS = get_joke_times(3);
 const DISTINC_JOKE_SUBMISSIONS = get_joke_times(3, false);
 const UNLISTED_KEYWORD = "Unlisted";
 
 /**
- * @returns {import("../types/Joke").Joke_Submission[]}
+ * @returns {import("../types/Joke").User_Submission[]}
  */
 function get_joke_times(n, duplicates = true) {
   let jokes = Array(n).fill({
     content: "Des is a Hund!",
     explanation: "They are like a dog!",
     submitted_by: "",
-    keywords: [LISTED_KEYWORD],
+    keywords: "Dog, Cat",
   });
   if (duplicates) {
     return jokes;
   }
   // Return joke array with distinct contents
-  jokes.forEach(function (joke_object) {
+  jokes.forEach(function(joke_object) {
     return (joke_object.content = joke_object.content + "a");
   });
   return jokes;
