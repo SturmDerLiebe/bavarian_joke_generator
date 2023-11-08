2f889231482d1cad2bde78a1dbb5699ebf4674d9
Christian Sturm <christiansturm.dachau@web.de>
2023-11-08 12:07:26 +0100

    Feat(API): Add submission endpoint

--- api/ssr/main.js
+++ api/ssr/main.js
@@ -3,8 +3,14 @@
 import express from "express";
 import db_read_joke from "../../helpers/database/read_joke_db.js";
 import { is_valid_keyword } from "../../helpers/api/get_joke.js";
-import { Client_Error, Not_Found_Error } from "../../library/Errors.js";
+import {
+  Client_Error,
+  Duplicate_Joke_Error,
+  Not_Found_Error,
+} from "../../library/Errors.js";
 import { SSR_PORT } from "../../constants/api.js";
+import db_create_joke from "../../helpers/database/create_joke_db.js";
+import { is_valid_joke_data } from "../../helpers/api/post_joke.js";
 
 const APP = express();
 
@@ -12,12 +18,13 @@ APP.set("view engine", "pug");
 APP.get("/", async function (req, res) {
   try {
     const KEYWORD = req.query.keyword;
+    // ~Validation
     if (!is_valid_keyword(KEYWORD)) {
       throw new Client_Error(
         `Your keyword "${KEYWORD}" did contain non english letters, numbers or other symbols`,
       );
     }
-
+    // Data Manipulation
     const JOKE_DATA = await db_read_joke(KEYWORD);
     if (!JOKE_DATA.length) {
       throw new Not_Found_Error(
@@ -36,6 +43,49 @@ APP.get("/", async function (req, res) {
     }
   }
 });
+
+APP.post(
+  "/submit",
+  express.urlencoded({ extended: false }),
+  async function (req, res) {
+    try {
+      // Validation
+      if (!is_valid_joke_data(req.body)) {
+        throw new Client_Error(
+          "Either your joke content or explanation contained invalid symbols or formatting",
+        );
+      }
+      const {
+        content,
+        explanation,
+        submitted_by,
+        keywords: keywords_string,
+      } = req.body;
+      const keywords = /,\s/.test
+        ? keywords_string.split(/,\s/)
+        : keywords_string; // Only split when multiple words
+      // Data Manipulation
+      const NEW_ID = await db_create_joke({
+        content,
+        explanation,
+        submitted_by,
+        keywords,
+      });
+      // Response
+      res.redirect(303, `../joke?id=${NEW_ID}`);
+    } catch (error) {
+      // Error Responses
+      if (error instanceof Duplicate_Joke_Error) {
+        res.writeHead(409, error.message).end();
+      } else if (error instanceof Client_Error) {
+        res.writeHead(400, error.message).end();
+      } else {
+        console.error(error);
+        res.writeHead(500).end();
+      }
+    }
+  },
+);
 
 APP.listen(SSR_PORT, () => {
   console.log(`Server Side Rendering on port ${SSR_PORT}`);
