59499bfa6307173d8a6440cdacb1536d931b4d41
Christian Sturm <christiansturm.dachau@web.de>
2023-11-08 17:08:49 +0100

    Types: Make joke id optional

--- types/Joke.js
+++ types/Joke.js
@@ -3,7 +3,7 @@
 /**
  * The joke type read from the database.
  * @typedef {Object} Joke
- * @property {string} id - The identifier of this joke. MySQL should return this as a string not BigInt
+ * @property {string} [id] - The identifier of this joke. MySQL should return this as a string not BigInt
  * @property {string} content - The content of this joke.
  * @property {string} explanation - The explanation if this joke.
  * @property {string|"Anonymous"} submitted_by - The user who submitted the joke.
