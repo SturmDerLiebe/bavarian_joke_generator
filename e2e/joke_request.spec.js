2e9a3690ebe51ac8fcf171a6fa5c652ac20e95f4
Christian Sturm <christiansturm.dachau@web.de>
2023-11-09 20:52:48 +0100

    Test: Add read after submission e2e tests

--- e2e/joke_request.spec.js
+++ e2e/joke_request.spec.js
@@ -3,11 +3,13 @@
 import { test, expect } from "@playwright/test";
 import {
   VALID_KEYWORD,
-  DISTINCT_JOKE_SUBMISSIONS,
   LISTED_KEYWORD,
+  DISTINCT_CREATE_ARGS,
+  UNLISTED_KEYWORD,
 } from "../constants/playwright";
 import delete_all_jokes_and_keywords from "../helpers/test/delete_jokes";
 import db_create_joke from "../helpers/database/create_joke_db";
+import { INSERT_OPTIONS } from "../constants/connections.js";
 
 test.describe("When requesting a joke on the home/root page on", function () {
   test("a valid keyword, the user is directed to another page", async function ({
@@ -39,39 +41,38 @@ test.describe("When requesting a joke on the home/root page on", function () {
 
 test.describe("A User requesting a joke from the home page via", function () {
   // BEFORE EACH:
-  test.beforeEach(async function () {
+  test.afterEach(async function () {
     await delete_all_jokes_and_keywords();
   });
   /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
-  // Fails until submit_jokes is implemented
-  test.fail(
-    "a keyword, is directed to a page showing associated existing jokes",
-    async function ({ page }) {
-      // GIVEN
-      for (let submission of DISTINCT_JOKE_SUBMISSIONS) {
-        await submit_jokes(submission);
-      }
-      // WHEN
-      await page.goto("/");
-      await page.locator("[for=keyword]").fill(LISTED_KEYWORD);
-      await page.getByTestId("gen-btn").click();
-      // THEN
-      expect(await page.locator("figure").all()).toHaveLength(
-        DISTINCT_JOKE_SUBMISSIONS.length,
-      );
-    },
-  );
-});
-/*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
-test("an unlisted keyword, is directed to a page showing that there are no jokes yet", async function ({
-  page,
-}) {
-  // GIVEN
-  // WHEN
-  await page.goto("/");
-  await page.locator("[for=keyword]").fill(LISTED_KEYWORD);
-  await page.getByTestId("gen-btn").click();
-  // THEN
-  // See if the page dislpays some kind of "Sorry, no content for this joke yet" page.
-  await expect(page.getByText("sorry")).toBeAttached();
+  test("a listed keyword, is directed to a page showing associated existing jokes", async function ({
+    page,
+  }) {
+    // GIVEN
+    await db_create_joke(DISTINCT_CREATE_ARGS[0], INSERT_OPTIONS);
+    await db_create_joke(DISTINCT_CREATE_ARGS[1], INSERT_OPTIONS);
+    await db_create_joke(DISTINCT_CREATE_ARGS[2], INSERT_OPTIONS);
+    // WHEN
+    await page.goto("/");
+    await page.locator("[for=keyword]").fill(LISTED_KEYWORD);
+    await page.getByTestId("gen-btn").click();
+    // THEN
+    await expect(page).toHaveTitle(`Bavarian ${LISTED_KEYWORD} jokes`);
+    expect(await page.locator("figure").all()).toHaveLength(
+      DISTINCT_CREATE_ARGS.length,
+    );
+  });
+  /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+  test("an unlisted keyword, is directed to a page showing that there are no jokes yet", async function ({
+    page,
+  }) {
+    // GIVEN
+    // WHEN
+    await page.goto("/");
+    await page.locator("[for=keyword]").fill(UNLISTED_KEYWORD);
+    await page.getByTestId("gen-btn").click();
+    // THEN
+    // See if the page dislpays some kind of "Sorry, no content for this joke yet" page.
+    await expect(page.getByText("sorry")).toBeAttached();
+  });
 });
