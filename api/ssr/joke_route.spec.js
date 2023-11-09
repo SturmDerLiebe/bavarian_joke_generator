05c101a79dae70158be8912d42d7c6bbca47de2d
Christian Sturm <christiansturm.dachau@web.de>
2023-11-09 20:55:27 +0100

    Test: Write test for /joke?id endpoint

---
+++ api/ssr/joke_route.spec.js
@@ -0,0 +1,34 @@
+"use strict";
+
+import { test, expect } from "@playwright/test";
+import { db_insert_joke } from "../../helpers/database/create_joke_db";
+import { DISTINCT_CREATE_ARGS } from "../../constants/playwright";
+
+test.afterEach(async () => {
+  await delete_all_jokes_and_keywords();
+});
+
+/*––––––––––––––––––––––––––––––– GET /joke?id ––––––––––––––––––––––––––––––––––––––*/
+test.describe("A user can see one joke by requesting it with", function() {
+  test("an existing id. They should receive a 200 code.", async function({
+    request,
+  }) {
+    // GIVEN
+    const INSERTED_ID = await db_insert_joke(DISTINCT_CREATE_ARGS);
+    // WHEN
+    const RESPONSE = await request.get(`/joke?id=${INSERTED_ID}`);
+    // THEN
+    expect(RESPONSE.status()).toBe(200);
+  });
+
+  test.fail(
+    "a non-existent id. They should receive a 404 code.",
+    async function({ request }) {
+      // GIVEN
+      // WHEN
+      const RESPONSE = await request.get(`/joke?id=1`);
+      // THEN
+      expect(RESPONSE.status()).toBe(404);
+    },
+  );
+});
