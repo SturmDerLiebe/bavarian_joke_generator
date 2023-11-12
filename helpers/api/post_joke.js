3ee9f406051eb591e4b8d47bbbdcf50c8d4c25a7
Christian Sturm <christiansturm.dachau@web.de>
2023-11-12 20:21:58 +0100

    Feat(Helpers):Use new RegEx import

--- helpers/api/post_joke.js
+++ helpers/api/post_joke.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { USERNAME_PATTERN } from "../../RegEx/username";
+
 /**
  * Checks if the user submitted data is valid.
  * @param {import("../../types/Joke").User_Submission} data - User submitted data.
@@ -9,12 +11,11 @@ function is_valid_joke_data({ content, explanation, keywords, submitted_by }) {
   const CONTENT_REGEX = /^(?:[A-Za-zöüäÖÜÄ0-9\.,:;'ʼ";„‟–—\-\?!]+\s?)+$/;
   const EXPLANATION_REGEX = /^(?:[A-Za-z0-9\.,:;'ʼ";„‟–—\-\?!]+\s?)+$/;
   const KEYWORDS_REGEX = /^[A-Za-z]+(?:,\s[A-Za-z]+)*$/;
-  const USER_REGEX = /^[A-Za-z0-9_\-–—]*$/;
   return (
     CONTENT_REGEX.test(content) &&
     EXPLANATION_REGEX.test(explanation) &&
     KEYWORDS_REGEX.test(keywords) &&
-    USER_REGEX.test(submitted_by)
+    USERNAME_PATTERN.test(submitted_by)
   );
 }
 
