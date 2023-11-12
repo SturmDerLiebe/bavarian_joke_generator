4400f0626d0dc67ea861be6df193fa46f4b6bdb7
Christian Sturm <christiansturm.dachau@web.de>
2023-11-12 20:24:45 +0100

    Feat(Constants): Add webauthn constants

---
+++ constants/auth.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * The port on which authentication happens.
+ * @readonly
+ */
+const AUTH_PORT = process.env.SSR_PORT;
+
+// Human-readable title for your website
+const RP_NAME = "Bavarian Joke Generator";
+// A unique identifier for your website
+const RP_ID = process.env.RP_ID;
+// The URL at which registrations and authentications should occur
+const ORIGIN = `${process.env.RP_SCHEMA}://${RP_ID}`;
+
+export { AUTH_PORT, RP_NAME, RP_ID, ORIGIN };
