3aef592e1035d7de98f246c0b4b4fe6d7a65761a
Christian Sturm <christiansturm.dachau@web.de>
2023-11-12 22:40:47 +0100

    Fix(Database): Change READ to SELECT

--- database/mysql/dev/1_jokes_accounts.dev.sql
+++ database/mysql/dev/1_jokes_accounts.dev.sql
@@ -14,10 +14,10 @@ GRANT INSERT ON bavarian_jokes.joke TO submitter;
 GRANT INSERT ON bavarian_jokes.jk_pair TO submitter;
 GRANT SELECT, INSERT, UPDATE ON bavarian_jokes.keyword TO submitter;
 
--- ———————————————————————— AUTHENTICATOR ————————————————————————————————————
+-- AUTHENTICATOR
 CREATE USER IF NOT EXISTS authenticator IDENTIFIED BY 'authenticatorPW';
 
-GRANT READ, INSERT, UPDATE ON bavarian_jokes.users TO authenticator;
+GRANT SELECT, INSERT, UPDATE ON bavarian_jokes.users TO authenticator;
 
 -- LOGGED_IN_USER:
 CREATE USER IF NOT EXISTS logged_in_user IDENTIFIED BY 'logged_in_userPW';
