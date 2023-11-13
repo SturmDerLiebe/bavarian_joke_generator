0971bba166770fa19aa4663e9afaa3906bdb5478
Christian Sturm <christiansturm.dachau@web.de>
2023-11-13 13:22:12 +0100

    Fix(Types): Create correct Database Insert Return type

---
+++ types/Database.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * This packet is returned when inserting into the db via mysql2
+ * - with a single insert and
+ * - over a single conneection.
+ * @typedef {[import("mysql2").ResultSetHeader, import("mysql2").FieldPacket[]]} Mysql2_Insertion_Return_Data
+ */
+
+export {};
