0dbf96c5fcd9b962a569536df2fd9dd3ea6096f7
Christian Sturm <christiansturm.dachau@web.de>
2023-11-14 22:14:22 +0100

    Feat(Types): Add type for selections from mysql

--- types/Database.js
+++ types/Database.js
@@ -3,8 +3,13 @@
 /**
  * This packet is returned when inserting into the db via mysql2
  * - with a single insert and
- * - over a single conneection.
+ * - over a single connection.
  * @typedef {[import("mysql2").ResultSetHeader, import("mysql2").FieldPacket[]]} Mysql2_Insertion_Return_Data
  */
 
+/**
+ * This packet is returned when reading from the db via mysql2
+ * - over a single connection.
+ * @typedef {[import("mysql2").RowDataPacket[], import("mysql2").FieldPacket[]]} Mysql2_Selection_Return_Data
+ */
 export {};
