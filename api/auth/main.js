fac76358420abdbf741c2278a78c85d4269a2d9a
Christian Sturm <christiansturm.dachau@web.de>
2023-11-14 22:22:32 +0100

    Fix(API): Add missing functionality to POST registration route

--- api/auth/main.js
+++ api/auth/main.js
@@ -1,17 +1,25 @@
 "use strict";
 
 import express from "express";
-import { Client_Error, Duplicate_Error } from "../../library/Errors.js";
+import cookieParser from "cookie-parser";
+import {
+  Client_Error,
+  Duplicate_Error,
+  Not_Found_Error,
+} from "../../library/Errors.js";
 import {
   generateRegistrationOptions,
   verifyRegistrationResponse,
 } from "@simplewebauthn/server";
-import { AUTH_PORT, RP_ID, RP_NAME } from "../../constants/auth.js";
+import { AUTH_PORT, ORIGIN, RP_ID, RP_NAME } from "../../constants/auth.js";
 import { is_valid_username } from "../../helpers/auth/get_auth_options.js";
 import { db_insert_user } from "../../helpers/database/user/db_insert.js";
 import { db_update_challange } from "../../helpers/database/user/db_update.js";
+import { db_select_challange } from "../../helpers/database/user/db_select.js";
 
 const APP = express();
+
+/* =================== Registration ========================================= */
 
 /* ——————————————————— GET /generate-registration-options?username —————————— */
 APP.get("/generate-registration-options", async function (request, response) {
@@ -49,6 +57,17 @@ APP.get("/generate-registration-options", async function (request, response) {
     // Response:
     response
       .setHeader("Content-Type", "application/json")
+      .cookie(
+        "user_data",
+        { id: NEW_USER_ID, username: USERNAME },
+        {
+          domain: process.env.RP_ID,
+          expires: 0, // SESSION cookie
+          httpOnly: true,
+          secure: process.env.NODE_ENV == "development" ? true : false,
+          sameSite: "strict",
+        },
+      )
       .status(200)
       .json(OPTIONS);
   } catch (error) {
@@ -61,14 +80,92 @@ APP.get("/generate-registration-options", async function (request, response) {
       response
         .writeHead(
           500,
-          "There has been an innternal issue while treating your authentication",
+          "There has been an internal issue while treating your authentication",
         )
         .end();
     }
   }
 });
 
-/* ——————————————————— GET /generate-registration-options?username —————————— */
+/* ——————————————————— POST /verify-registration —————————————————————————— */
+/**
+ * user_data cookie type
+ * @typedef {Object} User_Data_Cookie
+ * @property {string} id
+ * @property {string} username
+ */
+
+APP.post(
+  "/verify-registration",
+  cookieParser(),
+  express.json(),
+  async function (request, response) {
+    try {
+      /**
+       * @type {User_Data_Cookie}
+       */
+      const USER_DATA = request.cookies.user_data;
+      /*—————————————————————— Validation ——————————————————————————————————*/
+      if (!USER_DATA || !USER_DATA.id) {
+        throw new Client_Error("The value of your cookie is empty");
+      } else if (!/^\d+$/.test(USER_DATA.id)) {
+        throw new Client_Error(
+          "The id of your cookie contains forbidden symbols",
+        );
+      }
+      const { id: USER_ID } = USER_DATA;
+
+      const EXPECTED_CHALLANGE = await db_select_challange(USER_ID);
+      if (!EXPECTED_CHALLANGE) {
+        throw new Not_Found_Error(
+          "The user you are trying to identifiy as does not exist",
+        );
+      }
+
+      const BODY = request.body;
+      let verification;
+      try {
+        verification = await verifyRegistrationResponse({
+          response: BODY,
+          expectedChallenge: EXPECTED_CHALLANGE,
+          expectedOrigin: ORIGIN,
+          expectedRPID: RP_ID,
+        });
+      } catch (error) {
+        console.error(error);
+        return response.status(400).statusMessage(error.message).end();
+      }
+      if (!verification.verified) {
+        throw new Client_Error(
+          "There was an issue verifying your authentication",
+        );
+      }
+      /*—————————————————————— Response ——————————————————————————————————*/
+      response
+        // TODO: set auth cookie headers
+        .status(204)
+        .end("Successfully authenticated");
+
+      /*—————————————————————— Error Codes ——————————————————————————————————*/
+    } catch (error) {
+      if (error instanceof Not_Found_Error) {
+        response.writeHead(404, error.message).end();
+      } else if (error instanceof Client_Error) {
+        response.writeHead(400, error.message).end();
+      } else {
+        console.error(error);
+        response
+          .writeHead(
+            500,
+            "There has been an internal issue while treating your authentication",
+          )
+          .end();
+      }
+    }
+  },
+);
+
+/* ========================= Log in ======================================== */
 
 APP.listen(AUTH_PORT, () => {
   console.log(`Authentication running on port ${AUTH_PORT}`);
