40b1076be70c4d596c9865f87d91f41c337d47c9
Christian Sturm <christiansturm.dachau@web.de>
2023-11-15 12:32:58 +0100

    Fix(Database): Give necessary privileges to authenticator

--- database/mysql/dev/1_jokes_accounts.dev.sql
+++ database/mysql/dev/1_jokes_accounts.dev.sql
@@ -18,6 +18,7 @@ GRANT SELECT, INSERT, UPDATE ON bavarian_jokes.keyword TO submitter;
 CREATE USER IF NOT EXISTS authenticator IDENTIFIED BY 'authenticatorPW';
 
 GRANT SELECT, INSERT, UPDATE (current_challange) ON bavarian_jokes.users TO authenticator;
+GRANT SELECT, INSERT ON bavarian_jokes.authenticator TO authenticator;
 
 -- LOGGED_IN_USER:
 CREATE USER IF NOT EXISTS logged_in_user IDENTIFIED BY 'logged_in_userPW';
