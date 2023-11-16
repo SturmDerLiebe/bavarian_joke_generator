a3818851bf57b3088a4daa79aa5c777d9dafde0b
Christian Sturm <christiansturm.dachau@web.de>
2023-11-16 01:50:05 +0100

    Feat(RegEx): Add patterns for explanation and content

---
+++ RegEx/textarea.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * German words and regular punctuation seperated by spaces.
+ */
+const CONTENT_PATTERN = /^(?:[A-Za-zöüäÖÜÄ0-9\.,:;'ʼ";„“–—\-\?!]+\s?)+$/;
+
+/**
+ * English words and regular punctuation seperated by spaces.
+ */
+const EXPLANATION_PATTERN = /^(?:[A-Za-z0-9\.,:;'"“”–—\-\?!]+\s?)+$/;
+
+export { CONTENT_PATTERN, EXPLANATION_PATTERN };
