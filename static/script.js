5a3d6f8dfd13d6c6d4c77fc7e50e63a977e64b87
Christian Sturm <christiansturm.dachau@web.de>
2023-11-16 18:50:58 +0100

    Feat(UI): Add logic to start login after submit

--- static/script.js
+++ static/script.js
@@ -1,10 +1,70 @@
 "use strict";
 
+import { CONTENT_PATTERN, EXPLANATION_PATTERN } from "../RegEx/textarea.js";
+import { handle_login } from "./event_handlers/login.js";
 import { get_registration_handler } from "./event_handlers/registration.js";
 
 // import { startRegistration } from "@simplewebauthn/browser";
 
-// TODO: Add logic to authenticate via submitting a joke
+/* ================== Log In Elements ================================ */
+/** @type {HTMLFormElement} */
+const SUBMIT_JOKE_FORM = document.getElementById("submit_form");
+
+/** @type {HTMLTextAreaElement} */
+const CONTENT_AREA = document.getElementById("content");
+
+/** @type {HTMLTextAreaElement} */
+const EXPLANATION_AREA = document.getElementById("explanation");
+
+/** @type {HTMLInputElement} */
+const KEYWORDS_INPUT = document.getElementById("keywords");
+
+/** @type {HTMLInputElement} */
+const SUBMITTED_BY_INPUT = document.getElementById("submitter");
+
+/** @type {HTMLSpanElement} */
+const ERROR_SPAN = document.getElementById("login_error");
+
+SUBMIT_JOKE_FORM.addEventListener("submit", async function joke_handler(event) {
+  // Check Validity of textareas:
+  if (!CONTENT_PATTERN.test(CONTENT_AREA.value)) {
+    CONTENT_AREA.setCustomValidity(
+      "For Content only use German letters and punctuation!",
+    );
+    SUBMIT_JOKE_FORM.reportValidity();
+    // Set Timeout so the empty custom validity does not interfere with current event
+    setTimeout(() => {
+      CONTENT_AREA.setCustomValidity("");
+    }, 5000); // -> new submits will stay invalid for 5 seconds!
+    return;
+  }
+  if (!EXPLANATION_PATTERN.test(EXPLANATION_AREA.value)) {
+    EXPLANATION_AREA.setCustomValidity(
+      "For Explanation only use English letters and punctuation!",
+    );
+    SUBMIT_JOKE_FORM.reportValidity();
+    // Set Timeout so the empty custom validity does not interfere with current event
+    setTimeout(() => {
+      EXPLANATION_AREA.setCustomValidity("");
+    }, 5000);
+    return;
+  }
+
+  if (SUBMITTED_BY_INPUT.value) {
+    /* ————————————————— WebauthnLogin ————————————————————————————————————— */
+    try {
+      // Make sure to always call this after constraint validation was done via reportValidity()
+      event.preventDefault(); // For some unkown reason the following fetch calls ignore a preventDefault that comes afterwards
+      await handle_login(SUBMITTED_BY_INPUT.value, event);
+      SUBMIT_JOKE_FORM.submit();
+    } catch (error) {
+      ERROR_SPAN.innerText = error.message;
+      ERROR_SPAN.hidden = false;
+    }
+  }
+});
+
+/* ================== Registration Elements ================================ */
 
 /**
  * @type {HTMLParagraphElement}
