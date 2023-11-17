a1b703cee61b26a9b349f7f861765603103753c1
Christian Sturm <christiansturm.dachau@web.de>
2023-11-17 01:52:11 +0100

    Feat(API): Add Log in Endpoints

--- api/auth/main.js
+++ api/auth/main.js
@@ -8,7 +8,9 @@ import {
   Not_Found_Error,
 } from "../../library/Errors.js";
 import {
+  generateAuthenticationOptions,
   generateRegistrationOptions,
+  verifyAuthenticationResponse,
   verifyRegistrationResponse,
 } from "@simplewebauthn/server";
 import { AUTH_PORT, ORIGIN, RP_ID, RP_NAME } from "../../constants/auth.js";
@@ -17,6 +19,11 @@ import { db_insert_user } from "../../helpers/database/user/db_insert.js";
 import { db_update_challange } from "../../helpers/database/user/db_update.js";
 import { db_select_challange } from "../../helpers/database/user/db_select.js";
 import { db_insert_authenticator } from "../../helpers/database/authenticator/db_insert.js";
+import {
+  db_select_authenticators,
+  db_select_single_authenticator,
+} from "../../helpers/database/authenticator/db_select.js";
+import { db_update_counter } from "../../helpers/database/authenticator/db_update.js";
 
 const APP = express();
 
@@ -198,8 +205,161 @@ APP.post(
 /* ========================= Log in ======================================== */
 
 /* ———————————————— /generate-authentication-options?username —————————————— */
-// 1. Generate authentication options
-//APP.get();
+APP.get("/generate-authentication-options", async (request, response) => {
+  try {
+    const USERNAME = request.query.username;
+    /* —————————————— Conditional UI Response: ——————————————————————————————— */
+    if (!USERNAME) {
+      const OPTIONS = await generateAuthenticationOptions({
+        rpID: RP_ID,
+        userVerification: "preferred",
+      });
+      response
+        .setHeader("Content-Type", "application/json")
+        .status(200)
+        .json(OPTIONS);
+      return; // Don't proceed further
+    }
+
+    // Validation:
+    if (!is_valid_username(USERNAME)) {
+      throw new Client_Error(
+        `The username ${USERNAME} contained forbidden symbols`,
+      );
+    }
+
+    const [AUTHENTICATORS, USER_ID] = await db_select_authenticators(USERNAME);
+
+    // 1. Generate authentication options
+    const OPTIONS = await generateAuthenticationOptions({
+      rpID: RP_ID,
+      allowCredentials: AUTHENTICATORS.map((authenticator) => ({
+        id: authenticator.credentialID,
+        type: "public-key",
+        transports: authenticator.transports,
+      })),
+      userVerification: "preferred",
+    });
+
+    db_update_challange(USER_ID, OPTIONS.challenge);
+
+    // Response:
+    response
+      .setHeader("Content-Type", "application/json")
+      .cookie(
+        "user_data",
+        { id: USER_ID, username: USERNAME },
+        {
+          domain: RP_ID,
+          expires: 0, // SESSION cookie
+          httpOnly: true,
+          secure: process.env.NODE_ENV == "production" ? true : false,
+          sameSite: "strict",
+        },
+      )
+      .status(200)
+      .json(OPTIONS);
+  } catch (error) {
+    if (error instanceof Not_Found_Error) {
+      response.writeHead(404, error.message).end();
+    } else if (error instanceof Client_Error) {
+      response.writeHead(400, error.message).end();
+    } else {
+      console.error(error);
+      response
+        .writeHead(
+          500,
+          "There has been an internal issue while treating your authentication",
+        )
+        .end();
+    }
+  }
+});
+/* ———————————————— /verify-authentication ————————————————————————————————— */
+APP.post(
+  "/verify-authentication",
+  cookieParser(),
+  express.json(),
+  async (request, response) => {
+    try {
+      /**
+       * @type {User_Data_Cookie}
+       */
+      const USER_DATA = request.cookies.user_data;
+      /*—————————————————————— Validation ——————————————————————————————————*/
+      if (!USER_DATA || !USER_DATA.id) {
+        throw new Client_Error("The value of your cookie is empty");
+      } else if (!/^\d+$/.test(USER_DATA.id)) {
+        throw new Client_Error(
+          "The id of your cookie contains forbidden symbols",
+        );
+      }
+      const { id: USER_ID } = USER_DATA;
+
+      const EXPECTED_CHALLANGE = await db_select_challange(USER_ID);
+      if (!EXPECTED_CHALLANGE) {
+        throw new Not_Found_Error(
+          "The user you are trying to identifiy as does not exist",
+        );
+      }
+
+      const BODY = request.body;
+
+      const [AUTHENTICATOR, AUTHENTICATOR_ID] =
+        await db_select_single_authenticator(USER_ID, BODY.id);
+
+      if (!AUTHENTICATOR) {
+        throw new Not_Found_Error(
+          `Could not find authenticator ${BODY.id} for user ${USER_ID}`,
+        );
+      }
+
+      let verification;
+      try {
+        verification = await verifyAuthenticationResponse({
+          response: BODY,
+          expectedChallenge: EXPECTED_CHALLANGE,
+          expectedOrigin: ORIGIN,
+          expectedRPID: RP_ID,
+          authenticator: AUTHENTICATOR,
+        });
+      } catch (error) {
+        console.error(error);
+        response.statusMessage = error.message; // TODO: change this message
+        return response.status(400).end();
+      }
+      if (!verification.verified) {
+        throw new Client_Error(
+          "There was an issue verifying your authentication",
+        );
+      }
+      /*——————————— 3. Post-authentication responsibilities —————————————————————*/
+      const NEW_COUNTER = verification.authenticationInfo.newCounter;
+      await db_update_counter(AUTHENTICATOR_ID, NEW_COUNTER);
+
+      /*—————————————————————— Response ——————————————————————————————————*/
+      response
+        // TODO: set auth cookie headers
+        .status(204)
+        .end("Successfully authenticated");
+      /*—————————————————————— Error Codes ——————————————————————————————————*/
+    } catch (error) {
+      if (error instanceof Not_Found_Error) {
+        response.writeHead(404, error.message).end();
+      } else if (error instanceof Client_Error) {
+        response.writeHead(400, error.message).end();
+      } else {
+        console.error(error);
+        response
+          .writeHead(
+            500,
+            "There has been an internal issue while treating your authentication",
+          )
+          .end();
+      }
+    }
+  },
+);
 
 APP.listen(AUTH_PORT, () => {
   console.log(`Authentication running on port ${AUTH_PORT}`);
