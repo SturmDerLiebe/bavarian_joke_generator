81a236661b0bb25948527d09113494b48f42d0ca
Christian Sturm <christiansturm.dachau@web.de>
2023-11-17 11:52:02 +0100

    Fix(UI): Patch up conditional UI

--- static/conditional_ui.js
+++ static/conditional_ui.js
@@ -1,8 +1,17 @@
 "use strict";
+
+const { error } = require("console");
+
 // See https://simplewebauthn.dev/docs/packages/browser#browser-autofill-aka-conditional-ui
 const { startAuthentication } = SimpleWebAuthnBrowser;
-fetch("/auth/generate-authentication-options").then((options) => {
-  startAuthentication(options, true).catch((err) =>
+fetch("/auth/generate-authentication-options")
+  .then((options) => { options.json() })
+  .then((options) => {
+    startAuthentication(options, true)
+  })
+  .catch((err) => {
+    console.warn(err)
+  })
+  .finally(
     console.info("Conditional UI loaded"),
   );
-});
