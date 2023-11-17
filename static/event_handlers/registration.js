9dbfed0fe9471b9f2ac986725ef5f14a4b077560
Christian Sturm <christiansturm.dachau@web.de>
2023-11-17 11:58:53 +0100

    Fix(UI): Fix up error handling

--- static/event_handlers/registration.js
+++ static/event_handlers/registration.js
@@ -16,7 +16,11 @@ function get_registration_handler(username_input, register_btn, register_msg) {
       response = await fetch(
         `/auth/generate-registration-options?username=${username_input.value}`,
       );
-      if (!response.ok) {
+      if (response.status === 409) {
+        throw new Error(
+          "Your Username is sadly already taken. Try another one!",
+        );
+      } else if (!response.ok) {
         throw new Error(
           "There was an issue setting up your account. Please try again!",
         );
@@ -25,36 +29,29 @@ function get_registration_handler(username_input, register_btn, register_msg) {
       /* —————————————— 2. Submit registration options to the authenticator ——————*/
       attResp = await startRegistration(await response.json());
 
+      /* —— 3. Submit the authenticator's response to the Relying Party for verification ——*/
+      const verificationResp = await fetch("/auth/verify-registration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(attResp),
+      });
+
+      // Show UI appropriate for the `verified` status
+      if (verificationResp.ok) {
+        register_msg.textContent =
+          "You successfully created your profile. How about you try submitting a joke with your name?";
+        register_btn.remove();
+      } else {
+        register_msg.textContent = `We are sorry, but something went wrong! Response error: ${verificationResp.status}`;
+      }
       // Error Handling:
     } catch (error) {
-      if (response.status === 409) {
-        register_msg.textContent =
-          "Sorry, but the Username you chose was already taken. Please try a new one!";
-      } else {
-        register_msg.textContent = error;
-        // TODO: Delete username from db
-      }
+      register_msg.textContent = error;
+      // TODO: Delete username from db
       register_btn.textContent = "Try again";
       register_btn.focus();
-      return; // Don't continue
-    }
-
-    /* —— 3. Submit the authenticator's response to the Relying Party for verification ——*/
-    const verificationResp = await fetch("/auth/verify-registration", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(attResp),
-    });
-
-    // Show UI appropriate for the `verified` status
-    if (verificationResp.ok) {
-      register_msg.textContent =
-        "You successfully created your profile. How about you try submitting a joke with your name?";
-      register_btn.remove();
-    } else {
-      register_msg.textContent = `We are sorry, but something went wrong! Response error: ${verificationResp.status}`;
     }
   };
 }
