dd8965e6dbe3a06f55a46b778e02362e3d5ca41c
Christian Sturm <christiansturm.dachau@web.de>
2023-11-19 22:49:28 +0100

    Fix(Docker): Substitute Http and ssl ports for hardcoded values

--- playwright.config.js
+++ playwright.config.js
@@ -89,7 +89,7 @@ export default defineConfig({
       testDir: "./e2e",
       name: "e2e",
       use: {
-        baseURL: `http://127.0.0.1:${process.env.HTTP_PORT}`,
+        baseURL: `http://127.0.0.1:80`,
       },
     },
   ],
