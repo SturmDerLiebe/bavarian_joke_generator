9b8c9b5f611308575582d81463b3f834fbed3951
Christian Sturm <christiansturm.dachau@web.de>
2023-12-02 15:35:05 +0100

    Doc(RegEx): Add explanation for difference between patterns

--- RegEx/username.js
+++ RegEx/username.js
@@ -1,6 +1,8 @@
 "use strict";
 
+/** When submitting a joke, no username is also valid */
 const SUBMIT_USERNAME_PATTERN = /^[A-Za-z0-9_\-]*$/;
+/** When Signing up, no username is not valid */
 const AUTH_USERNAME_PATTERN = /^[A-Za-z0-9_\-]+$/;
 
 export { SUBMIT_USERNAME_PATTERN, AUTH_USERNAME_PATTERN };
