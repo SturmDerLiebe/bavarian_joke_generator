a1e915366fa9ffd63e6043f47feec2cbb9839e1f
Christian Sturm <christiansturm.dachau@web.de>
2023-12-03 17:40:17 +0100

    Fix(PNPM): Correct option

--- database/mysql/production/1_jokes_accounts.production.sql
+++ database/mysql/production/1_jokes_accounts.production.sql
@@ -1,5 +1,5 @@
 -- READER:
-CREATE USER IF NOT EXISTS reader IDENTIFIED BY 'amOcsWJcnsai';
+CREATE USER IF NOT EXISTS reader IDENTIFIED BY 'amOcsWJcAmvIWn';
 
 GRANT SELECT ON bavarian_jokes.joke TO reader;
 GRANT SELECT ON bavarian_jokes.jk_pair TO reader;
@@ -8,7 +8,7 @@ GRANT SELECT ON bavarian_jokes.users TO reader;
 GRANT UPDATE (times_searched) ON bavarian_jokes.keyword TO reader;
 
 -- SUBMITTER:
-CREATE USER IF NOT EXISTS submitter IDENTIFIED BY 'zvARGGsgnstrnNSOGFNK';
+CREATE USER IF NOT EXISTS submitter IDENTIFIED BY 'zvARGGsgnstrnNSOGLwmACI';
 
 GRANT INSERT ON bavarian_jokes.joke TO submitter;
 GRANT INSERT ON bavarian_jokes.jk_pair TO submitter;
@@ -16,7 +16,7 @@ GRANT SELECT, INSERT, UPDATE ON bavarian_jokes.keyword TO submitter;
 GRANT SELECT ON bavarian_jokes.users TO submitter;
 
 -- AUTHENTICATOR
-CREATE USER IF NOT EXISTS authenticator IDENTIFIED BY 'KJHiuHJKbBHfuWXcfVKY';
+CREATE USER IF NOT EXISTS authenticator IDENTIFIED BY 'KJHiuHJKbBHfuWXAChiw';
 
 GRANT SELECT, INSERT, UPDATE (current_challange) ON bavarian_jokes.users TO authenticator;
 GRANT SELECT, INSERT, UPDATE (counter) ON bavarian_jokes.authenticator TO authenticator;
